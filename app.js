63b62e605508ca5d292e21fe90f580d34188f0ac
nazarius9273827 <nazarbenevskij@gmail.com>
2025-12-28 12:11:49 +0200

    Add homework with classes and objects

---
+++ app.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class House {
+    constructor(key) {
+        this.door = false;
+        this.tenants = [];
+        this.key = key;
+    }
+    comeIn(person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Людина зайшла в будинок');
+        }
+        else {
+            console.log('Двері зачинені');
+        }
+    }
+}
+class MyHouse extends House {
+    openDoor(key) {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+            console.log('Двері відчинені');
+        }
+        else {
+            console.log('Невірний ключ');
+        }
+    }
+}
+class Key {
+    constructor() {
+        this.signature = Math.floor(Math.random() * 100000);
+    }
+    getSignature() {
+        return this.signature;
+    }
+}
+class Person {
+    constructor(key) {
+        this.key = key;
+    }
+    getKey() {
+        return this.key;
+    }
+}
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+house.openDoor(person.getKey());
+house.comeIn(person);
+//# sourceMappingURL=app.js.map
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Людина зайшла в будинок');
+    } else {
+      console.log('Двері зачинені');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log('Двері відчинені');
+    } else {
+      console.log('Невірний ключ');
+    }
+  }
+}
+
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.floor(Math.random() * 100000);
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
